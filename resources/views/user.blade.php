91277f8464a1a8330ef4f8ba7f3858c23bcd99e4
alifia256 <alifiaaazzahra28@gmail.com>
2025-03-11 22:00:42 +0700

    JS4 - Praktikum 2.3 (Retreiving Aggregrates)

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -1,23 +1,27 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <title>Data User</title>
-</head>
-<body>
-    <h1>Data User</h1>
-    <table border="1" cellpadding="2" cellspacing="0">
-        <tr>
-            <th>ID</th>
-            <th>Username</th>
-            <th>Nama</th>
-            <th>ID Level Pengguna</th>
-        </tr>
-        <tr>
-            <td>{{ $data->user_id }}</td>
-            <td>{{ $data->username }}</td>
-            <td>{{ $data->name }}</td>
-            <td>{{ $data->level_id }}</td>
-        </tr>
-    </table>
-</body>
+    <head>
+        <title>Data User</title>
+        <style>
+            table {
+                border-collapse: collapse;
+            }
+            th, td {
+                border: 1px solid black;
+                padding: 8px;
+                text-align: center;
+            }
+        </style>
+    </head>
+    <body>
+        <h2>Data User</h2>
+        <table>
+            <tr>
+                <th>Jumlah Pengguna</th>
+            </tr>
+            <tr>
+                <td>{{$data}}</td>
+            </tr>
+        </table>
+    </body>
 </html>
