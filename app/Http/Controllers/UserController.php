ebecc8e23f4b96f8c0d575d791cfa9a17063ad35
alifia256 <alifiaaazzahra28@gmail.com>
2025-03-11 19:54:14 +0700

    JS 4 - Praktikum 1 ($fillable)

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -12,12 +12,12 @@ class UserController extends Controller
     {
         // tambah data user dengan Eloquent Model
         $data = [
-            'username' => 'customer-2',
-            'name'=>'Pelanggan Kedua',
-            'password' => Hash::make('12345'),
-            'level_id' => 5
+            'level_id' => 2,
+            'username'=>'manager_tiga',
+            'name' => 'Manager 3',
+            'password' => Hash::make('12345')
         ];
-        UserModel::insert($data); //update data user
+        UserModel::create($data);
 
         // coba akses model UserModel
         $user = UserModel::all(); // ambil semua data dari tabel m_user
--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -12,7 +12,7 @@ class UserModel extends Model
     protected $table = 'm_user'; // Menentukan nama tabel yang digunakan oleh model ini
     protected $primaryKey = 'user_id'; // Menentukan primary key dari tabel yang digunakan
 
-    protected $fillable = ['user_id', 'username', 'name', 'level_id', 'password', 'created_at', 'updated_at'];
+    protected $fillable = ['level_id', 'username', 'name'];
 
     
 }
